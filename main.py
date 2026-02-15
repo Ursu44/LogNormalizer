e97a60825de4b64f28a25dfabdec24ed96238290
cosmin.aursulesei <cosmin-irimia.aursulesei@student.tuiasi.ro>
2026-02-15 13:44:02 +0200

    update log normalizer 1

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ for msg in consumer:
 
     key, tpl, cluster, is_new, gen, tid, features = process(log)
 
-    print("Sending normaized logs")
+    print("Sending normaized logs to Kafka producer")
     event = {
         "log": log,
         "syntax_key": key,
